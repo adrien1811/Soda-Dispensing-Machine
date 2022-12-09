fa492f28367c2a717df352c036b8a9d618012dd1
adrien1811 <88145636+adrien1811@users.noreply.github.com>
2022-12-09 15:57:29 +0700

    Update substractor.vhd

--- substractor.vhd
+++ substractor.vhd
@@ -1,1 +1,15 @@
-
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+entity Substractor is
+    GENERIC (N: INTEGER := 16);
+    port (
+        a,b,c: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+        different, borrow: OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0)
+    );
+end entity;
+architecture Arch of Substractor is
+begin
+    different <= a XOR b XOR c;
+    borrow <= (NOT a AND b) OR (b AND c) OR (NOT a AND c);
+end architecture;
